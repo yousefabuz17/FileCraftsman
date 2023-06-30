02e22ea1370c30d91a89154a5f45c38a2184fed1
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-06-30 19:18:27 -0400

    Class information added for now.

---
+++ craftsman.py
@@ -0,0 +1,17 @@
+import os
+from bs4 import BeautifulSoup
+
+# Class for webscraping
+#   This class will be used to scrape stories and random information from the web
+#   to be added into text files to be manipulated and practiced with
+    # 1. Method to request & parse contents from different webpages
+    # 2. Method to retrieve random information from webpages
+    # 3. Method to merge all contents into an organized json file
+    # 4. Get method to retrieve contents from json file
+
+# Class for Craftsman
+#   This class will be used to create random directories and text files
+#   to be used for practicing and learning
+    # 1. Method to create a specific set of numbered directories
+    # 2. Method to create a specific set of numbered text files
+    # 3. Method to check current directory
