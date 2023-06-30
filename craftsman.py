8a11a138a23ca91b126d022284b338635ad76ea7
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-06-30 19:25:44 -0400

    Update

--- craftsman.py
+++ craftsman.py
@@ -1,4 +1,5 @@
-import os
+import os, requests, json
+from pathlib import Path
 from bs4 import BeautifulSoup
 
 # Class for webscraping
@@ -12,6 +13,11 @@ from bs4 import BeautifulSoup
 # Class for Craftsman
 #   This class will be used to create random directories and text files
 #   to be used for practicing and learning
-    # 1. Method to create a specific set of numbered directories
-    # 2. Method to create a specific set of numbered text files
-    # 3. Method to check current directory
+    # 1. Method to check current directory
+        # 1a. If current directory is not Desktop, change to Desktop
+        # 1b. If current directory is Desktop, make the first testing directory
+    # 2. Method to create a specific set of numbered directories
+    # 3. Method to create a specific set of numbered text files
+        # 3a. Change to be set based on user input instead of hard coded
+    # 4. Method to create the directories first, then generate the text files
+        # 4a. Fetch the generated information from the webscraper class to be added onto the text files
