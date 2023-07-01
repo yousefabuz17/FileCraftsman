66b77400789921c981a396e884eef457869696cf
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-07-01 11:27:13 -0400

    Webscraper class started

--- craftsman.py
+++ craftsman.py
@@ -1,6 +1,7 @@
 import os, requests, json
 from pathlib import Path
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 # Class for webscraping
 #   This class will be used to scrape stories and random information from the web
@@ -9,6 +10,33 @@ from bs4 import BeautifulSoup
     # 2. Method to retrieve random information from webpages
     # 3. Method to merge all contents into an organized json file
     # 4. Get method to retrieve contents from json file
+    # 5. Links to parse from:
+            # https://www.cs.cmu.edu/~bingbin/
+            # https://www.gutenberg.org/cache/epub/71080/pg71080-images.html
+            # https://www.sciencedirect.com/topics/computer-science/research-paper
+class WebScraper:
+    def __init__(self, *url):
+        self.url = list(url)
+    
+    def get_p_contents(self):
+        contents_ = []
+        for i in self.url:
+            response = requests.get(i).text
+            soup = BeautifulSoup(response, 'html.parser').find_all('p')
+            for j in soup:
+                contents_.append(j.text.split())
+        full_contents = ''.join([' '.join(i) for i in contents_])
+        return self.add_word_count(full_contents)
+    
+    def add_word_count(self, words):
+        return f"{words}\n\n\033[1;4;33:47mWordCount:\033[0m {words.__len__()}"
+
+w = WebScraper('https://www.cs.cmu.edu/~bingbin/', 'https://www.gutenberg.org/cache/epub/71080/pg71080-images.html', 'https://www.sciencedirect.com/topics/computer-science/research-paper').get_p_contents()
+
+
+print(w)
+
+
 
 # Class for Craftsman
 #   This class will be used to create random directories and text files
