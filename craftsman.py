3b658a2d1087931248fbdb8c83758669df75ebb2
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-07-02 01:00:11 -0400

    Exporting data completed.

--- craftsman.py
+++ craftsman.py
@@ -1,23 +1,30 @@
-import os, requests, json
+import os
+import requests
+import json
+import glob
 import re
-from subprocess import call
 from pathlib import Path
 from bs4 import BeautifulSoup
-from pprint import pprint
 from uuid import uuid4
 
 
 class WebScraper:
     def __init__(self, *url):
         self.url = list(url)
-        self.all_contents = {}
         self.arg_length = self.url.__len__()
+        self.path_name = Path.cwd().name
+    
+    def get_url_id(self, index):
+        all_links = {key: value for key, value in enumerate(self.url)}
+        return all_links.get(index)
     
     def parse_url(self):
+        all_tags = []
         for urls in self.url:
             response = requests.get(urls).text
             soup = BeautifulSoup(response, 'html.parser')
-            all_tags = self.get_tag_info(soup)
+            tags = self.get_tag_info(soup)
+            all_tags.append(tags)
         return all_tags
     
     def get_tag_info(self, soup):
@@ -32,74 +39,41 @@ class WebScraper:
         return tag_info
     
     def export_tag_data(self, data):
+        json_dir = 'JSON'
         unique_file_uuid = str(uuid4())[-3:]
-        file_name = f'FileCraftsman/tag_data{unique_file_uuid}.json'
-        if not os.path.exists('FileCraftsman/tag_data.json'):
-            with open(file_name, 'w') as file:
+        org_file_name = f'{json_dir}/tag_data.json'
+        new_file_name = f'{json_dir}/tag_data{unique_file_uuid}.json'
+        if not os.path.exists('JSON'):
+            os.makedirs('JSON')
+        if not os.path.exists(org_file_name):
+            with open(org_file_name, 'w') as file:
                 json.dump(data, file, indent=4)
-                file.close()
-                CleanData(file_name).merge_tag_data()
+        else:
+            with open(new_file_name, 'w') as file:
+                json.dump(data, file, indent=4)
 
 class CleanData:
-    def __init__(self, file_name):
-        self.file_name = file_name
-
-    def merge_tag_data(self):
-        merged_data = {}
-        
-        with open(self.file_name, 'r') as file1:
-            data = json.load(file1)
-        
-        for key, value in data.items():
-            if key in merged_data:
-                data[key].extend(value)
-            else:
-                merged_data[key] = value
-        
-        with open(self.file_name, 'w') as f:
-            json.dump(merged_data, f, indent=4)
-            f.close()
-
-
-class RemoveFiles:
-    def __init__(self):
-        self.files = os.listdir()
-        self.cwd = Path.cwd()
+    def __init__(self, directory=None):
+        self.directory = directory
     
-    def fetch_all_files(self):
-        if self.cwd.name == 'Projects':
-            os.chdir('FileCraftsman')
-            for file in self.files:
-                if file.startswith('tag_data'):
-                    print(file)
-    
-    # def remove(self):
-    #     if self.cwd == self.path_name:
-    #         all_files = self.fetch_all_files()
-    #         print(max(all_files))
-    #     elif self.cwd != self.path_name:
-    #         os.chdir('FileCraftsman')
-    #         all_files = self.fetch_all_files()
-    #         print(all_files)
+    def all_json_files(self):
+        os.chdir('JSON')
+        return glob.glob('*.json')
 
 
-links = 'https://www.youtube.com/','https://www.cs.cmu.edu/~bingbin/','https://www.gutenberg.org/cache/epub/71080/pg71080-images.html','https://www.sciencedirect.com/topics/computer-science/research-paper','https://www.linkedin.com/'
-#,'https://www.cs.cmu.edu/~bingbin/','https://www.gutenberg.org/cache/epub/71080/pg71080-images.html','https://www.sciencedirect.com/topics/computer-science/research-paper','https://www.linkedin.com/'
-# w = WebScraper(*links).get_all_contents()
-w = WebScraper(*links).parse_url()
-print(RemoveFiles().fetch_all_files())
+def main():
+    links = ('https://www.youtube.com/', 'https://www.cs.cmu.edu/~bingbin/', 'https://www.gutenberg.org/cache/epub/71080/pg71080-images.html', 'https://www.sciencedirect.com/topics/computer-science/research-paper', 'https://www.linkedin.com/')
 
+    if links:
+        web_link_parser = WebScraper(*links)
+        parsed_data = web_link_parser.parse_url()
+        if parsed_data:
+            web_link_parser.export_tag_data(parsed_data) #Exports data into JSON folder
+            data = CleanData()
+            json_files = data.all_json_files()
+        else:
+            print("Error: No tag information found")
+    else:
+        print('Error: No links were provided')
 
-
-
-# Class for Craftsman
-#   This class will be used to create random directories and text files
-#   to be used for practicing and learning
-    # 1. Method to check current directory
-        # 1a. If current directory is not Desktop, change to Desktop
-        # 1b. If current directory is Desktop, make the first testing directory
-    # 2. Method to create a specific set of numbered directories
-    # 3. Method to create a specific set of numbered text files
-        # 3a. Change to be set based on user input instead of hard coded
-    # 4. Method to create the directories first, then generate the text files
-        # 4a. Fetch the generated information from the webscraper class to be added onto the text files
+main()
