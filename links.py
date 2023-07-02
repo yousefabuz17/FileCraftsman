3b424526892ca2222e69e2ddc9cfdef4596cd303
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-07-02 19:45:32 -0400

    Updated

---
+++ links.py
@@ -0,0 +1,13 @@
+
+LINKS = [
+    'https://www.youtube.com/',
+    'https://www.cs.cmu.edu/~bingbin/',
+    'https://www.gutenberg.org/cache/epub/71080/pg71080-images.html',
+    'https://www.sciencedirect.com/topics/computer-science/research-paper',
+    'https://www.linkedin.com/',
+    'https://www.python.org/',
+    'https://github.com/',
+    'https://www.nytimes.com/',
+    'https://www.wikipedia.org/',
+    'https://www.amazon.com/'
+]
