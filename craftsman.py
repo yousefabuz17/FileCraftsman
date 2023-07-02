5aedf23e5094e56acce66c9582af4bf5d3e265f5
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-07-02 01:56:01 -0400

    Cleaned up code before proceeding onto next class.

--- craftsman.py
+++ craftsman.py
@@ -2,31 +2,15 @@ import os
 import requests
 import json
 import glob
-import re
-from pandas import pandas as pd
 from pathlib import Path
 from bs4 import BeautifulSoup
 from uuid import uuid4
+import logging
 
 
 class WebScraper:
-    def __init__(self, *url):
-        self.url = list(url)
-        self.arg_length = self.url.__len__()
-        self.path_name = Path.cwd().name
-    
-    def get_url_id(self, index):
-        all_links = {key: value for key, value in enumerate(self.url)}
-        return all_links.get(index)
-    
-    def parse_url(self):
-        all_tags = []
-        for urls in self.url:
-            response = requests.get(urls).text
-            soup = BeautifulSoup(response, 'html.parser')
-            tags = self.get_tag_info(soup)
-            all_tags.append(tags)
-        return all_tags
+    def __init__(self, *urls):
+        self.urls = list(urls)
     
     def get_tag_info(self, soup):
         all_tags = soup.find_all(True)
@@ -36,59 +20,73 @@ class WebScraper:
             'tag_text': [i.text for i in all_tags if i.text],
             'tag_contents': list(filter(lambda k: k,[[j.text for j in tag.contents if j.text] for tag in all_tags]))
         }
-        self.export_tag_data(tag_info)
         return tag_info
     
-    def export_tag_data(self, data):
-        json_dir = 'JSON'
-        unique_file_uuid = str(uuid4())[-3:]
-        org_file_name = f'{json_dir}/tag_data.json'
-        new_file_name = f'{json_dir}/tag_data{unique_file_uuid}.json'
-        if not os.path.exists('JSON'):
-            os.makedirs('JSON')
-        if not os.path.exists(org_file_name):
-            with open(org_file_name, 'w') as file:
-                json.dump(data, file, indent=4)
-        else:
-            with open(new_file_name, 'w') as file:
-                json.dump(data, file, indent=4)
-
-class MergeData:
-    def __init__(self, directory=None):
-        self.directory = directory
+    def parse_url(self, url):
+        response = requests.get(url).text
+        soup = BeautifulSoup(response, 'html.parser')
+        return self.get_tag_info(soup)
     
-    def merge_all(self):
-        all_files = []
-        
-        if Path.cwd().name != self.directory:
-            os.chdir(self.directory)
-            
-            for json_file in glob.glob('*.json'):
-                with open(json_file, 'r') as file1:
-                    data = json.load(file1)
-                all_files.append(data)
-            
-            with open('all_data.json', 'w') as file2:
-                json.dump(all_files, file2, indent=4)
-        
-        return all_files
+    def parse_urls(self):
+        all_tags = []
+        for url in self.urls:
+            tags = self.parse_url(url)
+            all_tags.append(tags)
+        return all_tags
 
+
+class JSONExporter:
+    def __init__(self, directory):
+        self.directory = directory
+        self.json_dir = 'JSON'
+    
+    def create_directory(self):
+        if not os.path.exists(self.json_dir):
+            os.makedirs(self.json_dir)
+    
+    def generate_unique_filename(self):
+        unique_file_uuid = str(uuid4())[-3:]
+        return f'tag_data{unique_file_uuid}.json'
+    
+    def export_data(self, data):
+        self.create_directory()
+        filename = self.generate_unique_filename()
+        file_path = os.path.join(self.directory, filename)
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+    
+    def merge_json_files(self):
+        all_files = []
+        os.chdir(self.directory)
+        for json_file in glob.glob('*.json'):
+            with open(json_file, 'r') as file:
+                data = json.load(file)
+            all_files.append(data)
+            
+            with open('full_data.json', 'w') as file:
+                json.dump(all_files, file, indent=4)
+
+        return all_files
 
 
 def main():
     links = ('https://www.youtube.com/', 'https://www.cs.cmu.edu/~bingbin/', 'https://www.gutenberg.org/cache/epub/71080/pg71080-images.html', 'https://www.sciencedirect.com/topics/computer-science/research-paper', 'https://www.linkedin.com/')
 
     if links:
-        web_link_parser = WebScraper(*links)
-        parsed_data = web_link_parser.parse_url()
+        web_scraper = WebScraper(*links)
+        parsed_data = web_scraper.parse_urls()
         if parsed_data:
-            web_link_parser.export_tag_data(parsed_data) #Exports data into JSON folder
-            data = MergeData('JSON')
-            json_full_data = data.merge_all()
+            json_exporter = JSONExporter('JSON')
+            for data in parsed_data:
+                json_exporter.export_data(data)
+            json_full_data = json_exporter.merge_json_files()
             print(json_full_data)
         else:
             print("Error: No tag information found")
     else:
         print('Error: No links were provided')
 
-main()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
