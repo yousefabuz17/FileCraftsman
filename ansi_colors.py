054b4fa2062f7977789fd6fe66b8140a3b5c5b0a
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-07-04 23:59:55 -0400

    Colorized and implemented aiohttp exceptions

---
+++ ansi_colors.py
@@ -0,0 +1,30 @@
+CODE = {
+    'Font Color': {
+        'Black': '\033[30m',
+        'Red': '\033[31m',
+        'Green': '\033[32m',
+        'Yellow': '\033[33m',
+        'Blue': '\033[34m',
+        'Magenta': '\033[35m',
+        'Cyan': '\033[36m',
+        'White': '\033[37m'
+    },
+    'Background': {
+        'Black': '\033[40m',
+        'Red': '\033[41m',
+        'Green': '\033[42m',
+        'Yellow': '\033[43m',
+        'Blue': '\033[44m',
+        'Magenta': '\033[45m',
+        'Cyan': '\033[46m',
+        'White': '\033[47m'
+    },
+    'Text Style': {
+        'Bold': '\033[1m',
+        'Dim': '\033[2m',
+        'Underline': '\033[4m',
+        'Blink': '\033[5m',
+        'Reverse': '\033[7m'
+    },
+    'Reset': '\033[0m'
+}
