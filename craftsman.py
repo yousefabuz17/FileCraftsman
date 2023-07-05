02209d02b080451c7c4317d8bd8c8c4337d600ec
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-07-05 01:06:25 -0400

    In-Progress

--- craftsman.py
+++ craftsman.py
@@ -1,18 +1,18 @@
-from async_web_scraper import *
-from pathlib import Path
 import json
+from pathlib import Path
+from async_web_scraper import *
 
 class Craftsman:
-    def __init__(self):
-        self.json_file_path = Path.cwd()  / 'FileCraftsman' / 'full_data.json'
+    def __init__(self, json_file):
+        self.json_file = json_file
+        self.json_file_path = Path.cwd()  / 'FileCraftsman' / self.json_file
         self.json_data = json.load(open(self.json_file_path, 'r'))
 
     def get_data(self):
         return self.json_data
 
-
 def main():
-    craftsman = Craftsman().get_data()
+    craftsman = Craftsman('full_data.json').get_data()
 
 if __name__ == '__main__':
     asyncio.run(scraper_main()) # Run the async web scraper first to obtain the JSON file
